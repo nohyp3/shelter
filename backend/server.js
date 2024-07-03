c1a09657ae1e6737e8df452c0e457d19ba40d7f3
Andre Ke <84885039+nohyp3@users.noreply.github.com>
2024-07-03 17:27:40 -0400

    fixed typos

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,10 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // MongoDB connection
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.MAPBOX)
-console.log(process.env.DB_URL)
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+// console.log(process.env.MAPBOX)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 

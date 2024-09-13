311161a30d2fb8db57da013129de7aac99107e93
Andre Ke <84885039+nohyp3@users.noreply.github.com>
2024-09-12 21:35:53 -0400

    Update get_data.py

--- cron/api/get_data.py
+++ cron/api/get_data.py
@@ -85,4 +85,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        return "test"
+        self.wfile.write(b'Successful!')
